ada0804a1e920bf8eb192c93562127307efabcc1
Franco-M-Enzian <emf0000b@gmail.com>
2024-09-23 23:36:13 +0900

    微調整

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -10,7 +10,6 @@ const app = new Hono()
 // 出力例：
 // http://127.0.0.1:8787/pokemon/1025?pokemon_id=1
 // あなたが入力したポケモンの図鑑番号1は全1025匹のうちの一つです。
-
   app.get('/pokemon/:id', (c) => {
     const pokemon_id = c.req.query('pokemon_id')
     const id = c.req.param('id')
@@ -18,8 +17,9 @@ const app = new Hono()
     return c.text(`あなたが入力したポケモンの図鑑番号${pokemon_id}は全${id}匹のうちの一つです。`)
   })
 
-// Basic認証
 
+
+// Basic認証
 app.use(
   '/admin/*',
   basicAuth({
