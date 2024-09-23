0654919e515182385ca8d4b0fd40b627d683aaa1
Franco-M-Enzian <emf0000b@gmail.com>
2024-09-23 23:32:10 +0900

    Basic認証

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -1,10 +1,15 @@
 import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
 
 const app = new Hono()
 
   app.get('/pokemon/top', (c) => {
     return c.text(`ポケモン管理サイトへようこそ。現在登録されているポケモンの数は1025匹です。`)
   })
+
+// 出力例：
+// http://127.0.0.1:8787/pokemon/1025?pokemon_id=1
+// あなたが入力したポケモンの図鑑番号1は全1025匹のうちの一つです。
 
   app.get('/pokemon/:id', (c) => {
     const pokemon_id = c.req.query('pokemon_id')
@@ -13,8 +18,20 @@ const app = new Hono()
     return c.text(`あなたが入力したポケモンの図鑑番号${pokemon_id}は全${id}匹のうちの一つです。`)
   })
 
+// Basic認証
+
+app.use(
+  '/admin/*',
+  basicAuth({
+    username: 'user',
+    password: 'password',
+  })
+)
+
+app.get('/admin', (c) => {
+  return c.text('認証されました！')
+})
+
 export default app
 
-// 出力例：
-// http://127.0.0.1:8787/pokemon/1025?pokemon_id=1
-// あなたが入力したポケモンの図鑑番号1は全1025匹のうちの一つです。
+
