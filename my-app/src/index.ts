391dd7e122f95210427cf5ac8845493a5698b165
Franco-M-Enzian <emf0000b@gmail.com>
2024-09-23 23:24:12 +0900

    pokemon top page and pokemon_id

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -2,8 +2,19 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-  app.get('/', (c) => {
-    return c.text(`ポケモン管理サイトへようこそ。`)
+  app.get('/pokemon/top', (c) => {
+    return c.text(`ポケモン管理サイトへようこそ。現在登録されているポケモンの数は1025匹です。`)
+  })
+
+  app.get('/pokemon/:id', (c) => {
+    const pokemon_id = c.req.query('pokemon_id')
+    const id = c.req.param('id')
+    c.header('X-Message', 'Hi!')
+    return c.text(`あなたが入力したポケモンの図鑑番号${pokemon_id}は全${id}匹のうちの一つです。`)
   })
 
 export default app
+
+// 出力例：
+// http://127.0.0.1:8787/pokemon/1025?pokemon_id=1
+// あなたが入力したポケモンの図鑑番号1は全1025匹のうちの一つです。
